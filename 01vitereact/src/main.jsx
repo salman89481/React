0c4be7976c33a51f9b0218c1de5dda200a9fe033
salman89481 <salmanahmad89481@gmail.com>
2024-03-20 16:07:42 +0530

    3hrs react learned

---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+
+// function MyApp(){
+//   return (
+//     <div>
+//       <h1>Custom  App</h1>
+//     </div>
+//   )
+// }
+
+
+// const ReactElement = {
+//   type: 'a',
+//   props:{
+//     href:'https://google.com',
+//     target:'_blank'
+//   },
+//   children:'click me to visit google'
+// }
+
+
+const anotherElement = (
+  <a href="https://google.com" target ='_blank'>Visit Google</a>
+)
+
+const ReactElement = React.createElement(
+  'a',
+  {href: 'https://google.com' , target: '_blank'} ,
+  'click me to visit google'
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    // <MyApp />
+    <App />
+    // ReactElement
+
+);
