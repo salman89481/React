06f928407336265eb8919853ff68c014ae0a49a6
salman89481 <salmanahmad89481@gmail.com>
2024-03-26 15:01:19 +0530

    interview

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,22 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
-  let [counter , setCounter] = useState(15)
+
+  let [counter, setCounter] = useState(15)
   // let counter = 5
 
-  const addValue =()=>{
-    if (counter === 20){
-      return;
-    }
-    setCounter(counter+1)
+  const addValue = () => {
+    //   if (counter === 20){
+    //     return;
+    //   }
+    //   setCounter(counter+1)
+    // 
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
   }
 
-  const removeValue = ()=>{
-    if(counter === 0){
+
+  const removeValue = () => {
+    if (counter === 0) {
       return;
     }
-    setCounter(counter-1)
+    setCounter(counter - 1)
   }
 
 
@@ -27,9 +33,9 @@ function App() {
     <>
       <h1>chai aur react</h1>
       <h2>Counter value : {counter}</h2>
-      
+
       <button
-      onClick={addValue}>Add Value {counter}</button>
+        onClick={addValue}>Add Value {counter}</button>
       <br />
       <button onClick={removeValue}>remove value {counter}</button>
     </>
---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+      <Card username ="salman ahmad" btnText = "clickme"/>
+      <Card username =" ahmad" btnText = "visit me"/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
