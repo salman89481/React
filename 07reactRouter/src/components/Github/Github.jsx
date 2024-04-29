d47365584394ec39bdb34388afbaf5049cf7026e
salman89481 <salmanahmad89481@gmail.com>
2024-04-29 16:08:49 +0530

    7th project done

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import {useLoaderData} from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data , SetData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(response => response.json () )
+    //     .then((data)=>{
+    //         console.log(data);
+    //         SetData(data)
+    //     })
+
+    //  }, [])
+    console.log(data.followers);
+    
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Folllowers: {data.followers}
+    <img src={data.avatar_url} alt="Git Picture" width={300} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async()=>{
+        const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+        return response.json()
+}
